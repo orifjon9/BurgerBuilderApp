8d4fecf93c0e4bd20859ccc4116d6baa62ddde49
ORIFJON NARKULOV <orifjon9@gmail.com>
2019-06-18 22:37:31 -0600

    Setting up the Project

--- config/webpack.config.js
+++ config/webpack.config.js
@@ -401,6 +401,8 @@ module.exports = function(webpackEnv) {
               use: getStyleLoaders({
                 importLoaders: 1,
                 sourceMap: isEnvProduction && shouldUseSourceMap,
+                modules: true,
+                localIdentName: '[name]__[local]__[hash:base64:5]',
               }),
               // Don't consider CSS imports dead code even if the
               // containing package claims to have no side effects.
@@ -417,6 +419,7 @@ module.exports = function(webpackEnv) {
                 sourceMap: isEnvProduction && shouldUseSourceMap,
                 modules: true,
                 getLocalIdent: getCSSModuleLocalIdent,
+                localIdentName: '[name]__[local]__[hash:base64:5]',
               }),
             },
             // Opt-in support for SASS (using .scss or .sass extensions).
