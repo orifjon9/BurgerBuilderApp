c54673b4ca07db95ab93bfe95f646c9d1a8f3951
ORIFJON NARKULOV <orifjon9@gmail.com>
2019-06-23 22:54:17 -0600

    Calculate ingredients count

--- src/components/Burger/Burger.js
+++ src/components/Burger/Burger.js
@@ -5,10 +5,15 @@ import classes from './Burger.css';
 import BurgerIngredient from './BurgerIngredient/BurgerIngredient';
 
 const burger = (props) => {
-    const transformedIngredients = Object.keys(props.ingredients)
+    let transformedIngredients = Object.keys(props.ingredients)
         .map(idKey => {
             return [...Array(props.ingredients[idKey])].map((_, i) => <BurgerIngredient key={idKey + i} type={idKey} />);
-        });
+        })
+        .reduce((arr, el) =>  arr.concat(el), []);
+        
+        if(transformedIngredients.length === 0){
+            transformedIngredients = <p>Please start adding ingredients</p>;
+        }
 
     return (
         <div className={classes.Burger}>
