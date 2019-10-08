71beefde7725086ae42009d235def3f638c21a2f
ORIFJON NARKULOV <orifjon9@gmail.com>
2019-10-07 21:33:53 -0600

    Add Global Error Handler

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -6,6 +6,7 @@ import BuildControls from '../../components/Burger/BuildControls/BuildControls';
 import Modal from '../../components/UI/Modal/Modal';
 import OrderSummary from '../../components/Burger/OrderSummary/OrderSummary';
 import Spinner from '../../components/UI/Spinner/Spinner';
+import withErrorHandler from '../../components/UI/withErrorHandler/withErrorHandler';
 import axios from '../../axios-orders';
 
 
@@ -123,4 +124,4 @@ class BurgerBuilder extends Component {
     };
 }
 
-export default BurgerBuilder;
+export default withErrorHandler(BurgerBuilder, axios);
