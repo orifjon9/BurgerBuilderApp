2a5b47f9271f2b206089adb76a4c66986c1d8341
ORIFJON NARKULOV <orifjon9@gmail.com>
2019-12-25 06:41:08 -0700

    Configure Redux devtools

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
 import { Provider } from 'react-redux';
-import { createStore, applyMiddleware } from 'redux';
+import { createStore, applyMiddleware, compose } from 'redux';
 
 import './index.css';
 import App from './App';
@@ -21,8 +21,8 @@ const logger = store => {
         };
     };
 };
-
-const store = createStore(reducer, applyMiddleware(logger));
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+const store = createStore(reducer, composeEnhancers(applyMiddleware(logger)));
 
 const app = (
     <Provider store={store}>
