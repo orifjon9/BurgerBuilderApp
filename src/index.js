b2174c9f6605b1268bbd55c1642b519aa8898a22
ORIFJON NARKULOV <orifjon9@gmail.com>
2019-12-25 06:25:38 -0700

    Redux apply middleware

--- src/index.js
+++ src/index.js
@@ -2,14 +2,27 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
 import { Provider } from 'react-redux';
-import { createStore } from 'redux';
+import { createStore, applyMiddleware } from 'redux';
 
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 import reducer from './store/reducer';
 
-const store = createStore(reducer);
+
+const logger = store => {
+    return next => {
+        return action => {
+            console.log('[Middleware] action state', action);
+            const res = next(action);
+            
+            console.log('[Middleware] store state', store.getState());
+            return res;
+        };
+    };
+};
+
+const store = createStore(reducer, applyMiddleware(logger));
 
 const app = (
     <Provider store={store}>
